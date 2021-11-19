ba14a03fdd9608f043f500c0aaa0317693a15632
Raga <ragawilliam570@gmail.com>
2021-11-19 14:11:23 +0300

    Updated middleware project to use whitenoise

--- Learning_log/settings.py
+++ Learning_log/settings.py
@@ -158,5 +158,5 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     # Static asset configuration.
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
